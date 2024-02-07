beee574137d4c76a6a02bae375ea6aaca0e428fb
ANNEMARIE05 <annemarieagbonou@gmail.com>
2024-02-08 00:42:54 +0100

    mes changement apres critiques

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -11,7 +11,7 @@ function Hero(){
 */}
 
 <section
-  className="overflow-hidden bg-[url(https://images.unsplash.com/photo-1518655048521-f130df041f66?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-top bg-no-repeat"
+  className="overflow-hidden bg-[url(https://images.unsplash.com/photo-1571974096020-5c2775ac5d47?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-top bg-no-repeat"
 >
   <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
     <div className="mx-auto max-w-xl text-center">
