84ed1cd11158419b0b2d0200691df4911b87132b
ANNEMARIE05 <annemarieagbonou@gmail.com>
2024-02-08 00:36:08 +0100

    mes changement apres critiques

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -11,41 +11,38 @@ function Hero(){
 */}
 
 <section
-  className="overflow-hidden bg-[url(https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-top bg-no-repeat"
+  className="overflow-hidden bg-[url(https://images.unsplash.com/photo-1518655048521-f130df041f66?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-top bg-no-repeat"
 >
- <div
-    className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
-  ></div>
-
-  <div
-    className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
-  >
-    <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
-      <h1 className="text-3xl font-extrabold sm:text-5xl">
+  <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+    <div className="mx-auto max-w-xl text-center">
+      <h1 className="text-3xl text-rose-500 font-extrabold sm:text-5xl">
       Agbonou Kossiwa Anne Marie
       </h1>
 
-      <p className="mt-4 max-w-lg sm:text-xl/relaxed">
+      <p className="mt-4 sm:text-xl/relaxed">
       J'aime l'informatique et le developpement web c'est ma passion
       </p>
 
-      <div className="mt-8 flex flex-wrap gap-4 text-center">
+      <div className="mt-8 flex flex-wrap justify-center gap-4">
         <a
+          className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
           href="#"
-          className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
         >
           Telecharger CV
         </a>
 
         <a
+          className="block w-full rounded px-12 bg-red-50 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
           href="#"
-          className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
         >
           Engagez moi
         </a>
       </div>
     </div>
   </div>
+
+
+
 </section>
         </>
     )
